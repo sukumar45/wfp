a159c2b942967596b7157d2fd33d82c31125a57c
Sukumar Katamreddy <rockstar1432@gmail.com>
2021-02-26 06:07:24 +0000

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 
 function App() {
   return (
+    <> 
+    <Particles
+    className="particles-canvas"
+    params ={{
+      particles: {
+        number: {
+          value: 30,
+          density: {
+            eanbled: true,
+            value_area:900
+          } 
+        },
+        shape:{
+          type:"cirlce",
+          stroke:{
+            width: 6,
+            colour:"00f"
+          }
+        }
+       }
+     }} 
+    />
+
     <Navbar/>
+    <Header/>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from "../logo192.png";
+//import logo from "../logo-sm.png";
 
 // REACT FONTAWESOME IMPORTS
 
@@ -16,12 +16,12 @@ const Navbar = () => {
             
         
         <div className="container-fluid">
-            <a className="navbar-brand" href="#"> <img className ="logo" src= {logo} alt = "-logo-"/> </a>
+           {/* <a className="navbar-brand" href="#"> <img className ="logo" src= {logo} alt = "-logo-"/> </a> */}
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <FontAwesomeIcon icon = {faCoffee} style = {{color: "#fff"}} />
             </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav xl-auto">
+            <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
